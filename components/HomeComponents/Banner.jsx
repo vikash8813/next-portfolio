11784d3a90e4f84c9ecb3cb5e544dab961950318
vikash8813 <vikashjangir2301@gmail.com>
2024-12-25 22:01:42 +0530

    fix explore link in banner

--- components/HomeComponents/Banner.jsx
+++ components/HomeComponents/Banner.jsx
@@ -1,6 +1,6 @@
 import Typewriter from 'typewriter-effect';
 import BannerLayout from '../Common/BannerLayout';
-import { Link } from 'react-scroll';
+import Link from 'next/link'
 
 const Banner = () => {
     return (
@@ -29,7 +29,7 @@ const Banner = () => {
                                         </span> {"</"}<span className="text-Green sm:text-base xl:text-lg font-bold">div</span>{">"} </span>
                                 </div>
                             </div>
-                            <Link to='intro' spy={true} smooth={true} duration={500} offset={-50} className="button">Explore</Link>
+                            <Link href='/portfolio' spy={true} smooth={true} offset={-50} className="button">Explore</Link>
                         </div>
                         <div className="w-48 h-52 relative hidden md:block">
                             <img className='absolute top-8 w-full h-full' src="images/emoji.png" alt="emoji" />
